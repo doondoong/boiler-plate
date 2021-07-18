d892196557acd66695a17a051cb11e6001f39851
todaykim <pztdrd@naver.com>
2021-07-18 21:39:46 +0900

    최초 저장소에 등록

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+
+const mongoose = require('mongoose')
+mongoose
+    .connect(
+        "mongodb+srv://doondoong:Doondoong4845!@cluster0.33yta.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: false,
+        }
+    )
+    .then(() => console.log("MongoDB Connected..."))
+    .catch((err) => console.log(err));
+
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
