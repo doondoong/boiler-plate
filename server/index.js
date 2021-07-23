5f989ff20213405579a41802f1bf48912a9812f2
todaykim <pztdrd@naver.com>
2021-07-23 09:44:55 +0900

    up

--- server/index.js
+++ server/index.js
@@ -104,6 +104,6 @@ app.get("/api/users/logout", auth, (req, res) => {
     });
 });
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
