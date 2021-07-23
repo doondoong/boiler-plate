79a46be1832231b61a661b7028baec52d5017697
todaykim <pztdrd@naver.com>
2021-07-23 17:23:28 +0900

    up6.1`

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,13 +1,13 @@
-// const { createProxyMiddleware } = require("http-proxy-middleware");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
-// module.exports = function (app) {
-//     app.use(
-//         "/api",
+module.exports = function (app) {
+    app.use(
+        "/api",
 
-//         createProxyMiddleware({
-//             target: "http://localhost:5000",
+        createProxyMiddleware({
+            target: "https://myboiler-plate.herokuapp.com:443",
 
-//             changeOrigin: true,
-//         })
-//     );
-// };
+            changeOrigin: true,
+        })
+    );
+};
