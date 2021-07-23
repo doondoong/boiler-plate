627123a5ecaf0157341c794c0fb1f202c483de05
todaykim <pztdrd@naver.com>
2021-07-23 15:39:29 +0900

    up2000

--- client/src/App.js
+++ client/src/App.js
@@ -5,17 +5,17 @@ import LoginPage from "./components/views/LoginPage/LoginPage";
 import RegisterPage from "./components/views/RegisterPage/RegisterPage";
 
 function App() {
-  return (
-      <Router>
-          <div>
-              <Switch>
-                  <Route exact path="/" component={LandingPage} />
-                  <Route exact path="/login" component={LoginPage} />
-                  <Route exact path="/register" component={RegisterPage} />
-              </Switch>
-          </div>
-      </Router>
-  ); 
+    return (
+        <Router>
+            <div>
+                <Switch>
+                    <Route exact path="/" component={LandingPage} />
+                    <Route exact path="/login" component={LoginPage} />
+                    <Route exact path="/register" component={RegisterPage} />
+                </Switch>
+            </div>
+        </Router>
+    ); 
 }
 
 export default App;
